394fadd73f311c0e68671ddf7bd18c0999a7d7df
Felip Pellicer <felip.pellicer@gmail.com>
2025-05-03 16:44:16 +0200

    New experiment naming

--- qaoa_factorization/hamiltonians.py
+++ qaoa_factorization/hamiltonians.py
@@ -5,7 +5,7 @@ from utils import sympy_to_pennylane
 
 
 #============================================= Problem Hamiltonians =============================================#
-def default_problem_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
+def quadratic_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
     H = sp.expand(
         (
             N*sp.Symbol('I')
@@ -17,7 +17,7 @@ def default_problem_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
     return sympy_to_pennylane(H)
 
 
-def simplified_problem_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
+def linear_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
     H = sp.expand(
         N*sp.Symbol('I')
         - (sum(2**l * (sp.Symbol('I') - sp.Symbol(f'Z_{l}')) / 2 for l in range(1, nx+1)) + sp.Symbol('I'))
@@ -26,31 +26,7 @@ def simplified_problem_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
 
     return sympy_to_pennylane(H)
 
-def abs_problem_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
-    H = sp.expand(
-        N*sp.Symbol('I')
-        - (sum(2**l * (sp.Symbol('I') - sp.Symbol(f'Z_{l}')) / 2 for l in range(1, nx+1)) + sp.Symbol('I'))
-        * (sum(2**m * (sp.Symbol('I') - sp.Symbol(f'Z_{m+nx}')) / 2 for m in range(1, ny+1)) + sp.Symbol('I'))
-    )
-
-    H_abs = abs(sympy_to_pennylane(H).matrix())
-    return qml.pauli_decompose(H_abs)
-#================================================================================================================#
-
-
-#=============================================== Cost Hamiltonians ==============================================#
-def default_cost_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
-    H = sp.expand(
-        (
-            N*sp.Symbol('I')
-            - (sum(2**l * (sp.Symbol('I') - sp.Symbol(f'Z_{l}')) / 2 for l in range(1, nx+1)) + sp.Symbol('I'))
-            * (sum(2**m * (sp.Symbol('I') - sp.Symbol(f'Z_{m+nx}')) / 2 for m in range(1, ny+1)) + sp.Symbol('I'))
-        )**2
-    )
-
-    return sympy_to_pennylane(H)
-
-def abs_cost_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
+def abs_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
     H = sp.expand(
         N*sp.Symbol('I')
         - (sum(2**l * (sp.Symbol('I') - sp.Symbol(f'Z_{l}')) / 2 for l in range(1, nx+1)) + sp.Symbol('I'))
@@ -63,7 +39,7 @@ def abs_cost_H(N: int, nx: int, ny: int) -> qml.Hamiltonian:
 
 
 #============================================== Mixer Hamiltonians ==============================================#
-def default_mixer_H(num_qubits: int) -> qml.Hamiltonian:
+def standard_mixer_H(num_qubits: int) -> qml.Hamiltonian:
     mixer_H = sum(qml.PauliX(i) for i in range(num_qubits))
     return mixer_H
 #================================================================================================================#
