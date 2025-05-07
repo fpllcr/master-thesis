942db4df108c8ce782199ae2c549daeca5c1785c
Felip Pellicer <felip.pellicer@gmail.com>
2025-05-07 10:28:23 +0200

    Set experiments for N=39

---
+++ qaoa_factorization/experiments/copy_and_replace.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Parameters (edit these values as needed)
+ORIGINAL_NUM=35
+COPY_NUM=39
+
+# Directory containing the JSON files (use "." for current directory)
+DIR="."
+
+# Loop through all JSON files starting with "N${ORIGINAL_NUM}_"
+for file in "$DIR"/N${ORIGINAL_NUM}_*.json; do
+  # Skip if no files match
+  [ -e "$file" ] || continue
+
+  # Create new filename by replacing OLD_NUM with NEW_NUM
+  new_file="${file/N${ORIGINAL_NUM}_/N${COPY_NUM}_}"
+
+  # Replace "number": ORIGINAL_NUM with "number": COPY_NUM and save to new file
+  sed "s/\"number\":[[:space:]]*$ORIGINAL_NUM/\"number\": $COPY_NUM/g" "$file" > "$new_file"
+
+  echo "Created $new_file"
+done
