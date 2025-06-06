ad52ec492da91786b4db7fbc70a9fbe02bdcb20c
Felip Pellicer <felip.pellicer@gmail.com>
2025-06-06 09:40:31 +0200

    Refactor completed and thesis introduction

--- qaoa_factorization/main.py
+++ qaoa_factorization/main.py
@@ -2,47 +2,31 @@ from argparse import ArgumentParser
 from datetime import datetime
 import json
 import os
+import math
 import multiprocessing as mp
 
-from pennylane_qaoa_solver import PennylaneQAOASolver
-from numpy_qaoa_solver import NumpyQAOASolver
+import numpy as np
+from tqdm import tqdm
+
+from qaoa_solver import QAOASolver
 
 
-def main(number, layers, problem_hamiltonian, cost_hamiltonian,
-         optimizer_opts, device, experiment_name,
-         reps, cpus, verbose, strategy):
-    
-    if strategy == 'pennylane':
-        solver = PennylaneQAOASolver(
-            N=number,
-            p=layers,
-            problem_hamiltonian=problem_hamiltonian,
-            cost_hamiltonian=cost_hamiltonian,
-            optimizer_opts=optimizer_opts,
-            device=device
-        )
-    
-    elif strategy == 'numpy':
-        solver = NumpyQAOASolver(
-            N=number,
-            p=layers,
-            problem_hamiltonian=problem_hamiltonian,
-            cost_hamiltonian=cost_hamiltonian,
-            optimizer_opts=optimizer_opts
-        )
-    else:
-        print(f'ERROR: not implemented strategy {strategy}')
+OPTIMIZERS = ['Nelder-Mead', 'L-BFGS-B', 'BFGS', 'COBYLA']
 
-    _ = solver.run(
-        reps=reps,
-        save_results=True,
-        experiment=experiment_name,
-        cpus=cpus,
-        verbose=verbose
+
+def main(config):
+
+    solver = QAOASolver(
+        N=config['number'],
+        layers=config['layers'],
+        problem_hamiltonian=config['problem_hamiltonian'],
+        cost_hamiltonian=config['cost_hamiltonian'],
+        optimizer_method=config['optimizer'],
+        optimizer_opts=config.get('optimizer_opts'),
+        extended_qaoa=config['extended_qaoa']
     )
 
-    if(verbose):
-        print('Completed successfully')
+    solver.run(config)
 
 
 if __name__ == "__main__":
@@ -50,60 +34,85 @@ if __name__ == "__main__":
     parser.add_argument('-e', '--experiment')
     parser.add_argument('-b', '--batch', help='Batch processing for the experiments of the provided N')
     parser.add_argument('-a', '--all', action='store_true', help='In batch mode, if false, it only process new configs. If true, reprocess all.')
-    parser.add_argument('-r', '--reps', default=100, type=int, help='Number of times to run the program for each configuration')
     parser.add_argument('-c', '--cpus', default=1, type=int, choices=range(1, mp.cpu_count()), help='Number of CPUs to use')
+    parser.add_argument('-o', '--optimizers', default='all')
+    parser.add_argument('-E', '--extended', action='store_true', help='Whether to use extended QAOA or traditional QAOA')
+    parser.add_argument('-B', '--bounded', action='store_true')
+    parser.add_argument('-r', '--random', action='store_true', help='Whether gamma_0 and beta_0 are set to random float or not')
     parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('-s', '--strategy', choices=['pennylane','numpy'], required=True)
-    parser.add_argument('-d', '--device', default='default.qubit', help='Pennylane device to use')
 
     args = parser.parse_args()    
     experiment = args.experiment
     batch = args.batch
     all = args.all
-    reps = args.reps
     cpus = args.cpus
+    optimizers = args.optimizers
+    extended = args.extended
+    bounded = args.bounded
+    random_params = args.random
     verbose = args.verbose
-    strategy = args.strategy
-    device = args.device
+
+    if optimizers == 'all':
+        optimizers = OPTIMIZERS
+    else:
+        optimizers = optimizers.split(',')
+        for opt in optimizers:
+            assert opt in OPTIMIZERS
 
     experiments = []
 
     if experiment:
         experiments.append(experiment)
+    
     elif batch:
-        batch_experiments = [e.split('_conf.json')[0] for e in
-                             filter(lambda e: e.startswith(f'N{batch}'), os.listdir('experiments'))]
+        batch_experiments = [e.split('.json')[0] for e in
+                             filter(lambda e: e.startswith(f'N{batch}'), os.listdir('experiments/configs'))]
         
         if not all:
-            existing_results = os.listdir(f'experiments/{strategy}_results')
+            existing_results = os.listdir('experiments/results')
             batch_experiments = list(filter(lambda e: e not in existing_results, batch_experiments))
 
         
         experiments.extend(batch_experiments) 
         experiments.sort()
+    
+    elif all:
+        all_experiments = [e.split('.json')[0] for e in
+                           filter(lambda e: e.startswith('N'), os.listdir('experiments/configs'))]
+        experiments.extend(all_experiments)
+        experiments.sort()
+
     else:
         print('Either --experiment [-e] or --batch [-b] must be provided')
         exit(1)
 
-    for i, exp_name in enumerate(experiments):
-        with open(f'experiments/{exp_name}_conf.json', 'r') as f:
-            conf = json.load(f)
+    configs = []
+    for experiment in experiments:
+        for optimizer in optimizers:
+            with open(f'experiments/configs/{experiment}.json', 'r') as f:
+                conf = json.load(f)
+    
+                if not random_params:
+                    conf['initial_gamma'] = math.pi / 3
+                    conf['initial_beta'] = math.pi / 3
+                else:
+                    gamma_0, beta_0 = np.random.uniform(np.pi / 2, 3 * np.pi / 2, size=2)
+                    conf['initial_gamma'] = gamma_0
+                    conf['initial_beta'] = beta_0
 
-        optimizer_opts = conf.get('optimizer_opts', {})
 
-        time = datetime.now().time().strftime('%H:%M:%S')
-        print(f'{time} [{i+1}/{len(experiments)}] Started experiment {exp_name} with {reps} repetitions')
+                conf['verbose'] = verbose
+                conf['experiment'] = experiment
+                conf['optimizer'] = optimizer
+                conf['extended_qaoa'] = extended
+                conf['bounded'] = bounded
+
+                configs.append(conf)
+
+    with mp.Pool(processes=cpus) as pool:
+        pbar = tqdm(total=len(configs), unit='exp', disable=verbose,
+                        bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_inv_fmt}]")
         
-        main(
-            number=conf['number'],
-            layers=conf['layers'],
-            problem_hamiltonian=conf['problem_hamiltonian'],
-            cost_hamiltonian=conf['cost_hamiltonian'],
-            optimizer_opts=optimizer_opts,
-            device=device,
-            experiment_name=exp_name,
-            reps=reps,
-            cpus=cpus,
-            verbose=verbose,
-            strategy=strategy
-        )
+        for _ in pool.imap_unordered(main, configs):
+            pbar.update()
+            pbar.refresh()
---
+++ qaoa_factorization/qaoa_solver.py
@@ -0,0 +1,249 @@
+from datetime import datetime
+import json
+import math
+import os
+from time import time
+
+import numpy as np
+from scipy.optimize import minimize
+
+import hamiltonians as hamiltonians
+from utils import *
+
+
+UNBOUNDED_OPTS = ['BFGS']
+GRADIENT_FREE_OPTIMIZERS = ['Nelder-Mead', 'COBYLA']
+DEFAULT_LAMBDA = math.pi/3
+
+OPTIMIZER_MULTIPLIER = {
+    'Nelder-Mead': 2000,
+    'L-BFGS-B': 500,
+    'BFGS': 500,
+    'COBYLA': 4000
+}
+
+class QAOASolver:
+    def __init__(self, N: int, layers: int=1,
+                 problem_hamiltonian: str='quadratic_H',
+                 cost_hamiltonian: str='quadratic_H',
+                 optimizer_method: str='Nelder-Mead',
+                 optimizer_opts: dict=None,
+                 extended_qaoa: bool=False,
+                 bounded: bool=False):
+        """
+        Initialize QAOA solver for integer factorization.
+
+        :param N: Number to factorize
+        :param p: Number of QAOA layers
+        :param problem_hamiltonian_gen: Function to generate the problem Hamiltonian
+        :param cost_hamiltonian_gen: Cost function to evaluate solutions
+        :param optimizer_method: Optimization method of scipy.optimize.minimize for parameter tuning
+        :param optimizer_opts: Options for the optimization function
+        """
+        self.N = N
+        self.nx = ceil(log2(floor(sqrt(N)))) - 1
+        self.ny = ceil(log2(floor(N/3))) - 1
+        self.num_qubits = self.nx + self.ny
+        self.dim = 2 ** self.num_qubits
+        self.layers = layers
+
+        problem_hamiltonian_gen = getattr(hamiltonians, problem_hamiltonian)
+        cost_hamiltonian_gen = getattr(hamiltonians, cost_hamiltonian)
+
+        self.Hp = problem_hamiltonian_gen(self.N, self.nx, self.ny)
+        self.Ep = np.diag(self.Hp)
+        self.Hc = cost_hamiltonian_gen(self.N, self.nx, self.ny)
+        self.Ec = np.diag(self.Hc)
+
+        self.optimizer_method = optimizer_method
+        if optimizer_opts is None:
+            optimizer_opts = {}
+            if optimizer_method == 'Nelder-Mead':
+                optimizer_opts['adaptive'] = True
+        self.optimizer_opts = optimizer_opts
+        self.extended_qaoa = extended_qaoa
+        self.bounded = bounded
+
+        self.lbda = DEFAULT_LAMBDA
+        
+        
+    
+    def _qaoa_layer(self, psi, gamma, beta):
+        return apply_op(Rx(beta), apply_expiH(gamma, self.Ep, psi))
+    
+    def _qaoa_layer_and_derivatives(self, psi, gamma, beta):
+        psi = apply_expiH(gamma, self.Ep, psi)
+        dpsi_dgamma = 1j * (self.Ep * psi)
+        R = Rx(beta)
+        dpsi_dgamma = apply_op(R, dpsi_dgamma)
+        psi = apply_op(R, psi)
+        dpsi_dbeta = apply_sum_op(-0.5j * sigma_x, psi)
+        return psi, dpsi_dgamma, dpsi_dbeta
+
+    def _qaoa_state(self, gammas, betas):
+        psi = np.full(self.dim, 1 / np.sqrt(self.dim), dtype=np.complex128)
+        for gamma, beta in zip(gammas, betas):
+            psi = self._qaoa_layer(psi, gamma, beta)
+        return psi
+    
+    def _qaoa_state_and_derivatives(self, gammas, betas):
+        psi = np.full(self.dim, 1 / np.sqrt(self.dim), dtype=np.complex128)
+        dgammas = []
+        dbetas = []
+        for gamma, beta in zip(gammas, betas):
+            psi, dgamma, dbeta = self._qaoa_layer_and_derivatives(psi, gamma, beta)
+            dgammas = [self._qaoa_layer(v, gamma, beta) for v in dgammas] + [dgamma]
+            dbetas = [self._qaoa_layer(v, gamma, beta) for v in dbetas] + [dbeta]
+        return psi, dgammas, dbetas
+    
+    def _compute_cost(self, params):
+        p = int(len(params)/2)
+        psi = self._qaoa_state(params[:p], params[p:])
+        cost = np.vdot(psi, self.Ec * psi).real
+        return cost
+    
+    def _compute_cost_and_gradient(self, params):
+        p = int(len(params)/2)
+        psi, dgammas, dbetas = self._qaoa_state_and_derivatives(params[:p], params[p:])
+        cost = np.vdot(psi, self.Ec * psi).real
+
+        gradient = []
+        for dpsi in dgammas + dbetas:
+            grad_i = 2 * np.real(np.vdot(dpsi, self.Ec * psi))
+            gradient.append(grad_i)
+        gradient = np.array(gradient)
+
+        return cost, gradient
+    
+    def _qaoa_layer_extended(self, psi, gamma, beta, lbda=DEFAULT_LAMBDA):
+        return apply_op(Rx(beta) @ U1(lbda), apply_expiH(gamma, self.Ep, psi))
+    
+    def _qaoa_layer_and_derivatives_extended(self, psi, gamma, beta, lbda=DEFAULT_LAMBDA):
+        psi = apply_expiH(gamma, self.Ep, psi)
+        dpsi_dgamma = 1j * (self.Ep * psi)
+        R = Rx(beta) @ U1(lbda)
+        dpsi_dgamma = apply_op(R, dpsi_dgamma)
+        psi = apply_op(R, psi)
+        dpsi_dbeta = apply_sum_op(-0.5j * sigma_x, psi)
+        return psi, dpsi_dgamma, dpsi_dbeta
+    
+    def _make_lambdas(self, gammas, lbda):
+        return [0.0] * (len(gammas) - 1) + [lbda]
+
+    def _qaoa_state_extended(self, gammas, betas, lbda=DEFAULT_LAMBDA):
+        psi = np.full(self.dim, 1 / np.sqrt(self.dim), dtype=np.complex128)
+        lambdas = self._make_lambdas(gammas, lbda)
+        for gamma, beta, lbda in zip(gammas, betas, lambdas):
+            psi = self._qaoa_layer_extended(psi, gamma, beta, lbda)
+        return psi
+    
+    def _qaoa_state_and_derivatives_extended(self, gammas, betas, lbda=DEFAULT_LAMBDA):
+        psi = np.full(self.dim, 1 / np.sqrt(self.dim), dtype=np.complex128)
+        dgammas = []
+        dbetas = []
+        lambdas = self._make_lambdas(gammas, lbda)
+        for gamma, beta, lbda in zip(gammas, betas, lambdas):
+            psi, dgamma, dbeta = self._qaoa_layer_and_derivatives_extended(psi, gamma, beta, lbda)
+            dgammas = [self._qaoa_layer_extended(v, gamma, beta, lbda) for v in dgammas] + [dgamma]
+            dbetas = [self._qaoa_layer_extended(v, gamma, beta, lbda) for v in dbetas] + [dbeta]
+        return psi, dgammas, dbetas
+    
+    def _compute_cost_extended(self, params):
+        p = int(len(params)/2)
+        psi = self._qaoa_state_extended(params[:p], params[p:], self.lbda)
+        cost = np.vdot(psi, self.Ec * psi).real
+        return cost
+    
+    def _compute_cost_and_gradient_extended(self, params):
+        p = int(len(params)/2)
+        psi, dgammas, dbetas = self._qaoa_state_and_derivatives_extended(params[:p], params[p:], self.lbda)
+        cost = np.vdot(psi, self.Ec * psi).real
+
+        gradient = []
+        for dpsi in dgammas + dbetas:
+            grad_i = 2 * np.real(np.vdot(dpsi, self.Ec * psi))
+            gradient.append(grad_i)
+        gradient = np.array(gradient)
+
+        return cost, gradient
+    
+    def _single_run(self, p, initial_gammas, initial_betas):
+
+        result_i = {
+            'N': self.N,
+            'nx': self.nx,
+            'ny': self.ny,
+            'layers': p,
+            'initial_gammas': initial_gammas,
+            'initial_betas': initial_betas
+        }
+
+        self.optimizer_opts['maxiter'] = 2 * p * OPTIMIZER_MULTIPLIER[self.optimizer_method]
+
+        bounds = [(0,2*np.pi)]*p*2
+
+        if self.optimizer_method in GRADIENT_FREE_OPTIMIZERS and not self.extended_qaoa:
+            cost_fn = self._compute_cost
+        elif self.optimizer_method in GRADIENT_FREE_OPTIMIZERS and self.extended_qaoa:
+            cost_fn = self._compute_cost_extended
+        elif self.optimizer_method not in GRADIENT_FREE_OPTIMIZERS and not self.extended_qaoa:
+            cost_fn = self._compute_cost_and_gradient
+        elif self.optimizer_method not in GRADIENT_FREE_OPTIMIZERS and self.extended_qaoa:
+            cost_fn = self._compute_cost_and_gradient_extended
+
+        start_time = time()
+        res = minimize(
+            fun=cost_fn,
+            x0=initial_gammas + initial_betas,
+            method=self.optimizer_method,
+            options=self.optimizer_opts,
+            bounds=bounds if self.bounded and self.optimizer_method not in UNBOUNDED_OPTS else None,
+            jac=self.optimizer_method not in GRADIENT_FREE_OPTIMIZERS
+        )
+        end_time = time()
+        elapsed_time = end_time - start_time
+
+        gammas = res.x[:p].tolist()
+        betas = res.x[p:].tolist()
+        state = self._qaoa_state(gammas, betas)
+        state_str = [str(comp).removeprefix('(').removesuffix(')') for comp in state]
+
+        result_i.update({
+            'gammas': gammas,
+            'betas': betas,
+            'cost': float(res.fun),
+            'state': state_str,
+            'optimizer_time': elapsed_time,
+            'optimizer_success': res.success,
+            'optimizer_message': res.message
+        })
+
+        if self.optimizer_method in ['COBYLA']:
+            result_i['optimizer_success'] = bool(result_i['optimizer_success'])
+        else:
+            result_i.update({'optimizer_steps': res.nit})
+
+
+        return result_i
+        
+    
+    def run(self, conf):
+        dirpath = f"experiments/results/{conf['experiment']}"
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+
+        strftime = datetime.now().strftime('%Y%m%d%H%M%S%f')
+        results_path = f"{dirpath}/{conf['experiment']}_{strftime}.jsonl"
+        
+        gammas = [conf['initial_gamma']]
+        betas = [conf['initial_beta']]
+
+        for p in range(1, self.layers+1):
+            res = self._single_run(p, gammas, betas)
+
+            gammas = res['gammas'] + [math.pi / 3]
+            betas = res['betas'] + [math.pi / 3]
+
+            with open(results_path, 'a') as fout:
+                res['config'] = conf
+                fout.write(json.dumps(res) + '\n')
