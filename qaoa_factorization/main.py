3992d623a000211e51cb19016fbc6ddae1318d6b
Felip Pellicer <felip.pellicer@gmail.com>
2025-08-27 11:11:11 +0200

    Thesis (#13)

--- qaoa_factorization/main.py
+++ qaoa_factorization/main.py
@@ -12,13 +12,22 @@ from qaoa_solver import QAOASolver
 
 OPTIMIZERS = ['Nelder-Mead', 'L-BFGS-B', 'BFGS', 'COBYLA']
 
-INIT_PARAMS = {
-    'a': [2.278194341790279,1.8878746546919813],
-    'b': [3.4748376816697872, 2.118867272280805],
-    'c': [4.022993274680973, 0.8503996595801566]
-}
-
 repo = Repo('..')
+
+
+def main(config):
+
+    solver = QAOASolver(
+        N=config['number'],
+        layers=config['layers'],
+        problem_hamiltonian=config['problem_hamiltonian'],
+        cost_hamiltonian=config['cost_hamiltonian'],
+        optimizer_method=config['optimizer'],
+        optimizer_opts=config.get('optimizer_opts'),
+        extended_qaoa=config['extended_qaoa']
+    )
+
+    solver.run(config)
 
 
 if __name__ == "__main__":
@@ -27,7 +36,7 @@ if __name__ == "__main__":
     parser.add_argument('-b', '--batch', help='Batch processing for the experiments of the provided N')
     parser.add_argument('-a', '--all', action='store_true', help='In batch mode, if false, it only process new configs. If true, reprocess all.')
     parser.add_argument('-o', '--optimizers', default='all')
-    parser.add_argument('-i', '--init', help='Initial params gamma_0 and beta_0 separated by a comma or alphabetic for predefined sets')
+    parser.add_argument('-i', '--init', help='Initial params gamma_0 and beta_0 separated by a comma')
     parser.add_argument('-c', '--cont', help='Continue the experiment until the specified number of layers in its conf file.')
     parser.add_argument('-v', '--verbose', action='store_true')
 
@@ -102,9 +111,6 @@ if __name__ == "__main__":
     if initial_params is not None and ',' in initial_params:
         initial_params = [float(p) for p in initial_params.split(',')]
         gamma_0, beta_0 = initial_params
-    elif initial_params is not None:
-        assert initial_params in INIT_PARAMS.keys()
-        gamma_0, beta_0 = INIT_PARAMS[initial_params]
 
     for experiment in experiments:
         for optimizer in optimizers:
