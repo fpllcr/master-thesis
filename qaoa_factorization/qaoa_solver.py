379797b1483a146eea2802e070c01a65aacffdb6
Felip Pellicer <felip.pellicer@gmail.com>
2025-09-29 17:06:19 +0200

    Fixed X-rotation

--- qaoa_factorization/qaoa_solver.py
+++ qaoa_factorization/qaoa_solver.py
@@ -62,15 +62,16 @@ class QAOASolver:
         
     
     def _qaoa_layer(self, psi, gamma, beta):
-        return apply_op(Rx(2*beta), apply_expiH(gamma, self.Ep, psi))
+        # H_M = - sum X  ===> exp(-i beta H_M) = sum R_X(-2 beta)
+        return apply_op(Rx(-2*beta), apply_expiH(gamma, self.Ep, psi))
     
     def _qaoa_layer_and_derivatives(self, psi, gamma, beta):
         psi = apply_expiH(gamma, self.Ep, psi)
         dpsi_dgamma = -1j * (self.Ep * psi)
-        R = Rx(2*beta)
+        R = Rx(-2*beta)
         dpsi_dgamma = apply_op(R, dpsi_dgamma)
         psi = apply_op(R, psi)
-        dpsi_dbeta = apply_sum_op(-1j * sigma_x, psi)
+        dpsi_dbeta = apply_sum_op(1j * sigma_x, psi)
         return psi, dpsi_dgamma, dpsi_dbeta
     
     def _init_state(self):
@@ -87,10 +88,10 @@ class QAOASolver:
                 psi = np.kron(psi, next_ket)
             
         else:
-            # |------...>
-            psi = ket_minus
+            # |++++++...>
+            psi = ket_plus
             for i in range(1, self.num_qubits):
-                psi = np.kron(psi, ket_minus)
+                psi = np.kron(psi, ket_plus)
         
         return psi
 
